e38a66867cc5de22094938a6c0699a4f3dd97187
Jackie Crivlare <jcrivlare@ufl.edu>
2022-06-12 16:37:58 -0700

    assignment 5 final

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-//Selectors
-document.querySelector("form").addEventListener("submit", handleSubmitForm);
-document.querySelector("ul").addEventListener("click", handleClickDelete);
+let form = document.querySelector("form");
+form.onsubmit = handleSubmitForm;
 
-//Event Handlers
 function handleSubmitForm(e) {
   e.preventDefault();
   let input = document.querySelector("input");
-  if (input.value != "") addTodo(input.value);
-  input.value = " ";
+  let str = input.value.trim();
+  if (str.length != 0) addTodo(input.value);
+  input.value = "";
 }
 
-//Not add todo when input is only spaces (use .innerHTML?)
-function AddLi(str) {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(str));
-  li.innerHTML += ' <button onclick="this.parentNode.remove()">-</button>';
-  document.getElementById("out").appendChild(li);
+function handleClickDelete(e) {
+  if (this.style.textDecoration == "line-through") {
+    this.parentNode.removeChild(this);
+  } else {
+    this.style.textDecoration = "line-through";
+  }
 }
 
-//Helpers
-function addTodo(todo) {
-  let ul = document.querySelector("ul");
+function addTodo(str) {
+  let todoListUl = document.getElementById("todo-list");
   let li = document.createElement("li");
+  let button = document.createElement("button");
 
-  li.innerHTML = `
-    <span class="todo-item">${todo}</span>
-    `;
+  button.appendChild(document.createTextNode(str));
+  li.appendChild(button);
+  todoListUl.appendChild(li);
 
-  li.classList.add("todo-list-item");
-  ul.appendChild(li);
+  li.onclick = handleClickDelete;
 }
-
-//??? For completing items (strike through)
-li.onclick = function () {
-  var text = li.style.text - decoration.toggle(line - through);
-};
-
-//??? For removing items on double click
-listDiv.addEventListener("dblclick", (e) => {
-  if (e.target.tagName == "LI") {
-    e.target.classList.remove("marked");
-  }
-});
-
-removeItemButton.addEventListener("click", function (e) {
-  var marked = listDiv.querySelectorAll("li.marked");
-  for (var li of marked) {
-    var ul = li.parentNode;
-    ul.removeChild(li);
-  }
-});
